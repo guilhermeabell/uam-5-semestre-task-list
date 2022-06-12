80b46fccc2d401d2503b41e0b68e7ce850f03203
Handryc Natan <handryc2016@gmail.com>
2022-06-12 03:00:34 -0300

    make public path to serving only static files

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, 'ui/')));
+app.use(express.static(path.resolve(__dirname, 'public/')));
 
 app.listen(3000, () => {
     console.log('app listen on http://localhost:3000')
